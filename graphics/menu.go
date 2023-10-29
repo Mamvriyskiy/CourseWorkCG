04e3079a2d76e2deac8d56982acad32a44f65152
Mamvriyskiy <68384944+Mamvriyskiy@users.noreply.github.com>
2023-10-29 16:10:16 +0300

    create block menu

--- graphics/menu.go
+++ graphics/menu.go
@@ -10,61 +10,81 @@ import (
 )
 
 func CreateMenu(a fyne.App) *fyne.Container {
+	// Ввод размерности сцены
+	var entryA, entryB *widget.Entry
+	{
+		entryA = widget.NewEntry()
+		entryA.SetPlaceHolder("от 1 до 100")
+		entryB = widget.NewEntry()
+		entryB.SetPlaceHolder("от 1 до 100")
+	}
 
-	entryA := widget.NewEntry()
-	entryA.SetPlaceHolder("от 1 до 100")
-	entryB := widget.NewEntry()
-	entryB.SetPlaceHolder("от 1 до 100")
+	// Кнопки создания и отчистки сцены
+	var createSceneButton, clearButton *widget.Button
+	{
+		createSceneButton = widget.NewButton("Создать сцену", func() {
+		})
 
-	createSceneButton := widget.NewButton("Создать сцену", func() {
-	})
+		clearButton = widget.NewButton("Очистить", func() {
+		})
+	}
 
-	clearButton := widget.NewButton("Очистить", func() {
-	})
+	// Надписи в меню
+	var labelTextCamera, spacerText *widget.Label
+	{
+		labelTextCamera = widget.NewLabel("Управление камерой")
+		labelTextCamera.TextStyle = fyne.TextStyle{Bold: true, Italic: false}
 
-
-	labelTextCamera := widget.NewLabel("Управление камерой")
-	labelTextCamera.TextStyle = fyne.TextStyle{Bold: true, Italic: false}
-
-	spacerText := widget.NewLabel("")
+		spacerText = widget.NewLabel("")
+	}
 	spacer := container.NewCenter(spacerText)
-
-	buttonUpCamera := widget.NewButtonWithIcon("", theme.MoveUpIcon(), func() {
-        // Действие, выполняемое при нажатии на кнопку
-    })
-
-	buttonDownCamera := widget.NewButtonWithIcon("", theme.MoveDownIcon(), func() {
-        // Действие, выполняемое при нажатии на кнопку
-    })
-
-	buttonLeftCamera := widget.NewButtonWithIcon("", theme.NavigateBackIcon(), func() {
-        // Действие, выполняемое при нажатии на кнопку
-    })
-
-	buttonRightCamera := widget.NewButtonWithIcon("", theme.NavigateNextIcon(), func() {
-        // Действие, выполняемое при нажатии на кнопку
-    })
-
 	text := container.NewCenter(labelTextCamera)
 
-	btnDark := widget.NewButton("Темная тема", func() {
-		a.Settings().SetTheme(theme.DarkTheme())
-	})	
+	// Управление камерой
+	var buttonUpCamera, buttonDownCamera, buttonLeftCamera, buttonRightCamera *widget.Button
+	{
+		buttonUpCamera = widget.NewButtonWithIcon("", theme.MoveUpIcon(), func() {
+			// Действие, выполняемое при нажатии на кнопку
+		})
 
-	btnLight := widget.NewButton("Светлая тема", func() {
-		a.Settings().SetTheme(theme.LightTheme())
-	})	
+		buttonDownCamera = widget.NewButtonWithIcon("", theme.MoveDownIcon(), func() {
+			// Действие, выполняемое при нажатии на кнопку
+		})
 
-	menu := container.New(layout.NewVBoxLayout(), entryA, entryB, createSeparatorLine(1), createSceneButton, 
-		clearButton, createSeparatorLine(3), text, buttonUpCamera,
-		buttonLeftCamera, buttonRightCamera, buttonDownCamera,  createSeparatorLine(3), spacer, btnDark, btnLight)
+		buttonLeftCamera = widget.NewButtonWithIcon("", theme.NavigateBackIcon(), func() {
+			// Действие, выполняемое при нажатии на кнопку
+		})
+
+		buttonRightCamera = widget.NewButtonWithIcon("", theme.NavigateNextIcon(), func() {
+			// Действие, выполняемое при нажатии на кнопку
+		})
+	}
+
+	// Смена тем(светлый, темный)
+	var btnDark, btnLight *widget.Button
+	{	
+		btnDark = widget.NewButton("Темная тема", func() {
+			a.Settings().SetTheme(theme.DarkTheme())
+		})	
+
+		btnLight = widget.NewButton("Светлая тема", func() {
+			a.Settings().SetTheme(theme.LightTheme())
+		})	
+	}
+
+	menu := container.New(layout.NewVBoxLayout(), 
+		entryA, entryB, 
+		createSeparatorLine(1), createSceneButton, clearButton, 
+		createSeparatorLine(3), text, 
+		buttonUpCamera, buttonLeftCamera, buttonRightCamera, buttonDownCamera,  
+		createSeparatorLine(3), spacer, btnDark, btnLight)
 
 	return menu
 }
 
 func createSeparatorLine(width float32) *canvas.Line {
-    line := canvas.NewLine(theme.SeparatorColor()) // Используйте цвет темы или укажите свой цвет
-    line.StrokeWidth = width // Ширина линии
-    line.Resize(fyne.NewSize(100, 1)) // Размер линии (100 пикселей в ширину, 1 пиксель в высоту)
+    line := canvas.NewLine(theme.SeparatorColor())
+    line.StrokeWidth = width
+    line.Resize(fyne.NewSize(100, 1))
     return line
 }
